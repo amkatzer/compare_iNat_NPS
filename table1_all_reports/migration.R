817f67378b498d32ab916d2d917db3e42a9da01d
Amanda Katzer <amkat100@gmail.com>
2020-04-20 09:02:29 -0500

    small change to add variable for parkname outside of loop

--- table1_all_reports/migration.R
+++ table1_all_reports/migration.R
@@ -3,6 +3,7 @@
 library(stringr) #use str_extract from this package
 
 table3 <- read.csv("ACAD_table3.csv", stringsAsFactors = FALSE)
+parkname <- "ACAD"
 park_states <- read.csv("nps_boundary.csv", stringsAsFactors = FALSE)
 
 BISON_in_state <- rep(NA, length(table3$Scientific_name))
@@ -13,7 +14,7 @@ cutoff <- 50 #put in the cutoff amount that you want for entries in the state
 for (i in 1:length(table3$Scientific_name)){
   numberinBISON <- unlist(strsplit(table3$Occurences.in.BISON[i], split=";")) #splits up the BISON occurrences from table 3
   numberinBISON <- gsub("[[:space:]]", "", numberinBISON) #remove white spaces
-  parkstate <- park_states$STATE[park_states$UNIT_CODE == "ACAD"] #get park state from the database
+  parkstate <- park_states$STATE[park_states$UNIT_CODE == parkname] #get park state from the database
   statedata <- as.integer(str_extract(numberinBISON[pmatch(parkstate, numberinBISON)], "[0-9]+$")) 
   if (statedata <= cutoff) {
     speciesundercutoff$BISON_in_state[i] <- statedata
@@ -21,6 +22,6 @@ for (i in 1:length(table3$Scientific_name)){
 }
 
 speciesundercutoff <- na.omit(speciesundercutoff[speciesundercutoff$BISON_in_state != "NA",])
-
+speciesundercutoff
 
 
